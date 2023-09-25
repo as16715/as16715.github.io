267cbe72141067184c17c194ede1ce8947493fb6
as16715 <as16715@nyu.edu>
2023-09-25 21:33:50 +0400

    Uploading Index, script and styles

---
+++ script.js
@@ -0,0 +1,86 @@
+// Tabbed Menu
+function openMenu(evt, menuName) {
+    // Hide all elements with class "menu"
+    var i, x, tablinks;
+    x = document.getElementsByClassName("menu");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+
+    // Remove the "w3-dark-grey" class from all elements with class "tablink"
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < x.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" w3-dark-grey", "");
+    }
+
+    // Display the menu with the specified menuName
+    document.getElementById(menuName).style.display = "block";
+
+    // Add the "w3-dark-grey" class to the first child element of the clicked element (highlighting the tab)
+    evt.currentTarget.firstElementChild.className += " w3-dark-grey";
+}
+
+// Automatically click the "myLink" element to display the initial menu
+document.getElementById("myLink").click();
+
+// Text Rotation Animation
+var TxtRotate = function (el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtRotate.prototype.tick = function () {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    // Update the text content of the element with a wrapping span
+    this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+
+    var that = this;
+    var delta = 300 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+    }
+
+    // Schedule the next tick based on delta
+    setTimeout(function () {
+        that.tick();
+    }, delta);
+};
+
+// Initialize text rotation for elements with class "txt-rotate"
+window.onload = function () {
+    var elements = document.getElementsByClassName('txt-rotate');
+    for (var i = 0; i < elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-rotate');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+            new TxtRotate(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+
+    // Inject CSS for the text rotation animation
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+    document.body.appendChild(css);
+};
